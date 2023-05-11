289380e2dbccbd6b394b4fc7e36be8374b678308
Thomas Ekhardt <thomas.ekhardt@sidion.de>
2023-05-11 18:03:03 +0200

    create web engineering projekt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { TracksComponent } from './components/tracks/tracks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    TracksComponent
   ],
   imports: [
     BrowserModule,
